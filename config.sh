6a3b89970023d649e73c408457f5499325bd72e3
James McConnell <bins4us@hotmail.com>
2023-05-15 02:18:59 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,8 +2,8 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
-export BRANCH="twrp-11" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
+export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
 export DEVICE="RG353P" # codename used in device tree
@@ -11,7 +11,7 @@ export DT_LINK="https://github.com/TheWildJames/RG353V_TWRP" # device tree link
 export DT_BRANCH="main" # device tree branch
 export VENDOR="rockchip" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="twrp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
